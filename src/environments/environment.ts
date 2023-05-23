376eb1a732df3667b10cea65839e7dffd0df73f0
Tejaswi Anand <tejaswi.vta@gmail.com>
2023-05-23 11:25:43 +0530

    first

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyB3x2W_JSjnUCzYvFUiDp0-_AfT42iXG5s",
+    authDomain: "todov1-6fcf8.firebaseapp.com",
+    projectId: "todov1-6fcf8",
+    storageBucket: "todov1-6fcf8.appspot.com",
+    messagingSenderId: "909057395433",
+    appId: "1:909057395433:web:bf427fe4146a8482a77e78"
+  }
+};
+
